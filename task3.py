e997a409b436a2fa080447f0cd24d4f846709b18
John-rudol <johnathan.rudolph097@students.cmcss.net>
2026-03-06 07:39:48 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -4,3 +4,24 @@ if not guard_awake:
 else:
     outcome = "Doom: you got caught while running across the field."
 print(outcome)
+
+"""
+
+  __
+         /\ `.
+         2^)_|
+         \`/(_
+           )/,`.
+        __((  \/
+       /.--|_.L\\_
+           \, \ \=
+           / ,/L
+          7 (\ \...,_
+          | | \____| )
+          ]_|      `\)
+          /_)  SK
+         `"
+"""
+
+
+
